bdd96635790afee7730cc7b35bb40ef8ecf323e3
JiYongKim <colorful8315@gmail.com>
2023-10-18 13:30:25 +0900

    Revert "refactor(SignUpController) : SignUpService interface application completed"
    This reverts commit 832fe26bb5e569a0dd2137b548d645495772ae92.

--- spring web server/meeting/src/main/java/zoom/meeting/service/signUp/implement/SignUpServiceImplementV1.java
+++ spring web server/meeting/src/main/java/zoom/meeting/service/signUp/implement/SignUpServiceImplementV1.java
@@ -1,19 +1,22 @@
 package zoom.meeting.service.signUp.implement;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import zoom.meeting.domain.member.Member;
 import zoom.meeting.domain.repositoryInterface.MemberRepository;
 import zoom.meeting.service.signUp.SignUpService;
 
 @Service
-@Transactional
-@RequiredArgsConstructor
 public class SignUpServiceImplementV1 implements SignUpService {
 
     private final MemberRepository memberRepository;
+    private final TransactionTemplate txTemplate;
 
+    public SignUpServiceImplementV1(MemberRepository memberRepository, PlatformTransactionManager txManager) {
+        this.memberRepository = memberRepository;
+        this.txTemplate = new TransactionTemplate(txManager);
+    }
 
     @Override
     public boolean[] validation(String loginId, String password, String nickName) {
@@ -23,11 +26,19 @@ public class SignUpServiceImplementV1 implements SignUpService {
         // index = 3, ID password 동일 검증
         // index = 4, nickName 중복
         boolean[] overlapCheck = new boolean[5];
-        overlapCheck[0] = loginId.contains(" ");
-        overlapCheck[1] = nickName.contains(" ");
-        overlapCheck[2] = memberRepository.allLoginId().contains(loginId);
-        overlapCheck[3] = loginId.equals(password);
-        overlapCheck[4] = memberRepository.allNickName().contains(nickName);
+
+        txTemplate.executeWithoutResult((status) -> {
+            try {
+                overlapCheck[0] = loginId.contains(" ");
+                overlapCheck[1] = nickName.contains(" ");
+                overlapCheck[2] = memberRepository.allLoginId().contains(loginId);
+                overlapCheck[3] = loginId.equals(password);
+                overlapCheck[4] = memberRepository.allNickName().contains(nickName);
+            } catch (Exception e) {
+                throw  new IllegalStateException(e);
+            }
+        });
+
         for (boolean b : overlapCheck) {
             if(b){
                 return overlapCheck;
@@ -38,7 +49,13 @@ public class SignUpServiceImplementV1 implements SignUpService {
 
     @Override
     public Member memberSignUp(Member member) {
-        memberRepository.save(member);
+        txTemplate.executeWithoutResult(status -> {
+            try{
+                memberRepository.save(member);
+            }catch (Exception e){
+                throw new IllegalStateException(e);
+            }
+        });
         return member;
     }
 }
