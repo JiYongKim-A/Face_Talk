ea7b4765c15265ef8a42265acbc1022f61adeaca
JiYongKim <colorful8315@gmail.com>
2023-10-26 14:18:40 +0900

    feat(ConnectionConstForTest) : Add connection const for testing

---
+++ spring web server/meeting/src/test/java/zoom/meeting/ConnectionConstForTest.java
@@ -0,0 +1,7 @@
+package zoom.meeting;
+
+public abstract class ConnectionConstForTest {
+    public static final String URL = "";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+}
